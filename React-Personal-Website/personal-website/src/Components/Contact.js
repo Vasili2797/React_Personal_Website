fd7dbe04b0f0952ac62966fe2f3e331b4be09dfe
Vasili2797 <nashvilliv1@gmail.com>
2023-12-25 17:21:37 -0500

    Added Components

---
+++ React-Personal-Website/personal-website/src/Components/Contact.js
@@ -0,0 +1,82 @@
+import { Col, Row } from "react-bootstrap";
+
+const standardPanel = (contentVariable) => {
+  return (
+    <Row>
+      <Col xs={0} md={1}></Col>
+      <Col className="content-background">{contentVariable}</Col>
+      <Col xs={0} md={1}></Col>
+    </Row>
+  );
+};
+
+const Contact = () => {
+  console.log("Thank You");
+  const componentContent = (
+    <>
+      <Row className="justify-content-center mt-5">
+        <Col xs={12} sm={6} md={8}>
+          <h2>Contact</h2>
+          <p>
+            Aspiring developer interested in helping companies make their dreams
+            come true and achieve their full potential. For all the services,
+            please include your name, email address, and reason for contact and
+            I will get back as soon as possible!
+          </p>
+          <br />
+          <form
+            action="https://formsubmit.co/bjornbjornssonn@gmail.com"
+            method="POST"
+          >
+            <label for="name" id="name">
+              name
+            </label>
+            <input
+              name="name"
+              value=""
+              type="name"
+              id="name"
+              placeholder="name"
+              required
+            />
+
+            <div>
+              <label for="email" id="email">
+                email address
+              </label>
+              <input
+                name="email"
+                value=""
+                type="email"
+                id="email"
+                pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"
+                placeholder="email@..."
+                title="Not Valid"
+                required
+              />
+            </div>
+
+            <br />
+            <label for="Services" id="Services">
+              services required
+            </label>
+            <br />
+            <textarea
+              name="Service"
+              cols="50"
+              rows="10"
+              placeholder="Services "
+              required
+            ></textarea>
+            <br />
+            <button type="submit" id="submit">
+              Submit
+            </button>
+          </form>
+        </Col>
+      </Row>
+    </>
+  );
+  return standardPanel(componentContent);
+};
+export default Contact;
---
+++ React-Personal-Website/personal-website/src/Components/Navigation.js
@@ -0,0 +1,78 @@
+import Container from "react-bootstrap/Container";
+import {
+  Navbar,
+  Nav,
+  Dropdown,
+  DropdownButton,
+  ButtonGroup,
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import * as data from "../JSON/navigationLinks.json";
+
+const LinksString = JSON.stringify(data);
+const links = JSON.parse(LinksString).links;
+
+const Navigation = () => {
+  return (
+    <Navbar
+      expand="lg"
+      bg="dark"
+      data-bs-theme="dark"
+      className="bg-body-tertiary"
+    >
+      <Container>
+        <Navbar.Brand href="Home" className="navbar-brand">
+          Personal Webpage of Vasili Nashvilli
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Link to="Home" className="nav-link">
+              Home{" "}
+            </Link>
+
+            <Link to="Home" className="nav-link">
+              About
+            </Link>
+            <Link to="MyProjects" className="nav-link">
+              My Projects{" "}
+            </Link>
+
+            <Link
+              to="https://github.com/Vasili2797/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="nav-link"
+            >
+              GitHub{" "}
+            </Link>
+
+            <Link
+              to="https://www.linkedin.com/in/vasili-nashvilli-08bb95140/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="nav-link"
+            >
+              LinkedIn{" "}
+            </Link>
+
+            <Link
+              to="https://drive.google.com/file/d/1SbdxKqy4sT_Xih89vMHgVXDRR87Jm7qp/view?usp=sharing"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="nav-link"
+            >
+              Resume
+            </Link>
+
+            <Link to="Contact" className="nav-link">
+              Contact
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Navigation;
