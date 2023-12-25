11f6e5e9dc9a7573366eb7f1cf721458c77023cd
Vasili2797 <nashvilliv1@gmail.com>
2023-12-25 17:24:11 -0500

    Added App.css, App.js MyProjects.css and index.css

---
+++ React-Personal-Website/personal-website/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Introduction from "./Components/Introduction";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <Introduction />
+    </>
+  );
+}
+
+export default App;
