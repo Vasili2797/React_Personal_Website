54b014e31268ad0bd301ef62715cf4fe2c429120
Vasili2797 <nashvilliv1@gmail.com>
2023-12-25 19:48:09 -0500

    Changed the component name from Introduction to Route

--- React-Personal-Website/personal-website/src/App.js
+++ React-Personal-Website/personal-website/src/App.js
@@ -1,12 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Introduction from "./Components/Introduction";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Route from "./Components/Route";
 
 function App() {
   return (
     <>
-      <Introduction />
+      <Route />
     </>
   );
 }
