e43b1bee67a4db7ba5b00d262d468aba95d97e06
Vasili2797 <nashvilliv1@gmail.com>
2024-02-28 13:26:19 -0500

    Deleted self-taught developer line in introductory paragraph

--- React-Personal-Website/personal-website/src/Pages/About.js
+++ React-Personal-Website/personal-website/src/Pages/About.js
@@ -40,7 +40,7 @@ const aboutMeSection = () => {
         economics uniting the differences between the sciences of mathematics
         and finance. I am interested in different types of technologies and how
         it could improve the lives of many people in different fields of work. I
-        am a self-taught developer with knowledge in the following tech stack:
+        am a developer with knowledge in the following tech stack:
       </p>
       <ResponsiveAutoExample />
     </>
