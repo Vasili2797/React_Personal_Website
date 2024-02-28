03c29dc667a274efd64bb76ed58ec495a0a0ca3a
Vasili2797 <nashvilliv1@gmail.com>
2024-02-28 13:20:24 -0500

    Added the shopping list application

---
+++ React-Personal-Website/personal-website/src/Projects/ShoppingList.js
@@ -0,0 +1,52 @@
+import { Image } from "react-bootstrap";
+
+const ShoppingList = () => {
+  return (
+    <div className="figure-container">
+      <figure>
+        <ul>
+          <a
+            href="https://github.com/Vasili2797/ShoppingList/tree/main/shoppingListApp/shopping/src"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="image">
+              <Image
+                src="https://images.pexels.com/photos/1087727/pexels-photo-1087727.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                style={{ height: "300px", width: "450px", display: "block" }}
+                fluid
+                alt="React Personal Webstite image"
+              />
+              <figcaption>shopping list application</figcaption>
+
+              <div className="image__overlay">
+                <div className="image__title">Shopping Application</div>
+                <p className="image__description">
+                  The source code for the user friendly shopping applicatiion
+                  built using React JavaScript Library and Bootstrap Framework
+                </p>
+              </div>
+            </div>
+          </a>
+          <div>
+            <a
+              className="right"
+              href="https://reactshoppinglistapplication.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={() => {
+                console.log(
+                  "When Clicked, it will send the person to live display"
+                );
+              }}
+            >
+              Live
+            </a>
+          </div>
+        </ul>
+      </figure>
+    </div>
+  );
+};
+
+export default ShoppingList;
